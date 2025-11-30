e2656c1e58392fc00eda210bb19136a8ef478aac
riz254 <harunnjihia455@gmail.com>
2025-11-30 17:56:45 +0300

    HOME :zap:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,10 @@ export default function Home() {
       <Intro />
       <Destinations />
       <GreenwiseDifference />
+      <Itineraries />
       <Services />
       <Process />
       <Counter />
-      <Itineraries />
       <Footer />
     </>
   );
