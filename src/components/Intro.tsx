e5b2289169681dad0ac03b8dd388f435aafffe60
riz254 <harunnjihia455@gmail.com>
2025-12-07 22:58:39 +0300

    Intro :construction:

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -76,71 +76,83 @@ export default function CascadingImageStack() {
     // </div>
 
     <>
-
-     <div className="!mx-auto max-w-4/6 !pb-20 text-center">
+      <div className="!mx-auto max-w-4/6 !pb-20 text-center">
         <h2 className="!text-4xl md:text-4xl font-bold text-green-950 !mb-6">
- We welcome you to walk in our footsteps for the greatest African
-              safari and adventure experience.            </h2>
-                 <p className="max-w-4/5!mx-auto text-black !mb-6">
+          We welcome you to walk in our footsteps for the greatest African
+          safari and adventure experience.{" "}
+        </h2>
+        <p className="scale-105 !mx-auto text-black !mb-6">
+          We craft tailor-made sustainable journeys across Africa&apos;s top
+          destinations. From wildlife to culture, we have something for every
+          passion. With trusted partners worldwide, we ensure your comfort and
+          peace of mind, even in the remotest corners. Our expert team
+          customizes each safari program to meet your unique expectations. Let
+          us create an unforgettable experience just for you – one that sets
+          your journey apart. Explore Africa&apos;s wonders with us!
+        </p>
+      </div>
 
-We craft tailor-made sustainable journeys across Africa&apos;s top destinations. From wildlife to culture, we have something for every passion. With trusted partners worldwide, we ensure your comfort and peace of mind, even in the remotest corners. Our expert team customizes each safari program to meet your unique expectations.
-
-Let us create an unforgettable experience just for you – one that sets your journey apart. Explore Africa&apos;s wonders with us!
-
- 
-
-            </p>
-</div>
-
-
-    <section className="relative bg-green-50 !mt-32 max-w-full !px-6 md:!px-10 lg:!px-20">
-
-
-
-      <div className="!mx-auto flex flex-col-reverse md:flex-row items-center !p-0 gap-12 max-w-8xl">
-        {/* Right: Text Content */}
-        <div className="w-full !p-8 md:w-1/2 -top-[100px] relative  ">
-<div className=" relative ">
+      <section className="relative bg-green-50 !mt-32 max-w-full !px-6 md:!px-10 lg:!px-20">
+        <div className="!mx-auto flex flex-col-reverse md:flex-row items-center !p-0 gap-12 max-w-8xl">
+          {/* Right: Text Content */}
+          <div className="w-full !p-8 md:w-1/2 -top-[50px] relative  ">
+            <div className=" relative ">
               <h2
-              className="text-left  font-semibold text-green-700 text-xl md:text-2xl mb-2"
-              style={{ margin: "10px" }}
-            >
-              Discover Africa, with Africans!
-            </h2>
-            <p
-              className="mt-2 text-left text-lg md:text-2xl font-medium leading-snug text-gray-800"
-              style={{ margin: "10px" }}
-            >
-              We are natives of this land and we have spent our entire life here, discovering Africa’s best-kept secrets.
+                className="text-left font-extrabold text-green-800 text-xl md:text-2xl !mb-5"
+                style={{ margin: "10px" }}
+              >
+                Discover Africa, with Africans!
+              </h2>
+              <p
+                className="mt-2 text-left text-lg md:text-2xl font-medium !leading-10 text-gray-800 !mb-5"
+                style={{ margin: " 10px" }}
+              >
+                 We are proud natives of this land, storytellers, explorers, and passionate guardians of Africa’s hidden gems. 
+          For generations, we’ve roamed its wild plains, vibrant cities, and untouched wilderness, gathering insight 
+          that only true locals can offer.
 
+              </p>
+            </div>
 
-            </p>
-          </div>
+            <div>
+              <h2
+                className="text-left font-extrabold text-green-800 text-xl md:text-2xl !mb-5"
+                style={{ margin: "10px" }}
+              >
+                We take the initiative,so you can travel stress-free
+              </h2>
+              <h2
+                className="text-left font-extrabold text-green-800 text-xl md:text-2xl"
+                style={{ margin: "10px" }}
+              >
+                {/* so you can travel stress-free */}
+              </h2>
+              <p
+                className="mt-2 text-left text-lg md:text-2xl font-medium !leading-10 text-gray-800 !mb-5"
+                style={{ margin: "10px" }}
+              >
+                 From hand-picked destinations to seamless logistics, our expert team crafts personalized, private, 
+          and sustainable safari experiences designed around you. All you need to do is relax, we handle the rest.
+    
+              </p>
 
-          <div>
-            <h2
-              className="text-left font-extrabold text-green-800 text-xl md:text-2xl"
-              style={{ margin: "10px" }}
-            >
-              We take the initiative,so you can travel stress-free
+                  <div className="!mt-10" style={{ margin: "10px" }}>
+          <a 
+            href="/about"
+            className="inline-block    bg-gradient-to-r from-green-400 via-green-500 to-lime-400 bg-[length:200%_200%] !text-emerald-950 text-lg
+            font-black rounded-2xl 
+            !px-8 !py-3
+            shadow-md
+            transition-all duration-500 ease-in-out
+            will-change-transform
+            hover:scale-103 hover:shadow-lg hover:text-xl hover:shadow-lime-950 hover:bg-[position:right_center]"
+          >
+            Read More
+          </a>
+        </div>
+            </div>
 
-            </h2>
-            <h2
-              className="text-left font-extrabold text-green-800 text-xl md:text-2xl"
-              style={{ margin: "10px" }}
-            >
-              {/* so you can travel stress-free */}
-            </h2>
-            <p
-              className="mt-2 text-left text-lg md:text-2xl font-medium leading-snug text-gray-800"
-              style={{ margin: "10px" }}
-            >
-              Our experts are ready to plan your private, personalized, and
-              sustainable African safari experiences!
-            </p>
-          </div>
-
-          {/* <div>
+            {/* <div>
             <h2
               className="text-left text-xl md:text-2xl font-semibold text-lime-700"
               style={{ margin: "10px" }}
@@ -149,41 +161,41 @@ Let us create an unforgettable experience just for you – one that sets your jo
               safari and adventure experience.
             </h2>
           </div> */}
-        </div>
-        {/* Left: Stacked Images */}
-        <div className="relative w-3/4 md:w-1/2 h-[400px] flex justify-center md:justify-start">
-          {/* First Image */}
-          <div className="absolute top-[-25%] left-1/2 -translate-x-1/2 w-[70%] h-[70%]">
-            <Image
-              src="/images/images/kenya-giraffe-manor.jpeg"
-              alt="Safari Experience"
-              fill
-              className="object-cover rounded-xl shadow-xl"
-            />
           </div>
+          {/* Left: Stacked Images */}
+          <div className="relative w-3/4 md:w-1/2 h-[400px] flex justify-center md:justify-start">
+            {/* First Image */}
+            <div className="absolute top-[-25%] left-1/2 -translate-x-1/2 w-[70%] h-[70%]">
+              <Image
+                src="/images/images/kenya-giraffe-manor.jpeg"
+                alt="Safari Experience"
+                fill
+                className="object-cover rounded-xl shadow-xl"
+              />
+            </div>
 
-          {/* Second Image */}
-          <div className="absolute top-[30%] left-[40%] w-[60%] h-[60%]">
-            <Image
-              src="/images/images/Nairobi.png"
-              alt="Cultural Experience"
-              fill
-              className="object-cover rounded-xl shadow-2xl"
-            />
-          </div>
+            {/* Second Image */}
+            <div className="absolute top-[30%] left-[40%] w-[60%] h-[60%]">
+              <Image
+                src="/images/images/Nairobi.png"
+                alt="Cultural Experience"
+                fill
+                className="object-cover rounded-xl shadow-2xl"
+              />
+            </div>
 
-          {/* Third Image */}
-          <div className="absolute top-[35%] left-[-0%] w-[50%] h-[50%]">
-            <Image
-              src="/images/images/kenya-diani-beach.jpg"
-              alt="Adventure Experience"
-              fill
-              className="object-cover rounded-xl shadow-lg"
-            />
+            {/* Third Image */}
+            <div className="absolute top-[35%] left-[-0%] w-[50%] h-[50%]">
+              <Image
+                src="/images/images/kenya-diani-beach.jpg"
+                alt="Adventure Experience"
+                fill
+                className="object-cover rounded-xl shadow-lg"
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 }
